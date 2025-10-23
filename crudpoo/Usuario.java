3e38b56b4fac51f84e396287a6a9ef9e171264a8
Elias Merlo <elias.merlo.q@gmail.com>
2025-10-23 18:45:36 -0300

    crudpoo

--- crudpoo/Usuario.java
+++ crudpoo/Usuario.java
@@ -2,9 +2,10 @@ public class Usuario {
     private int id;
     private String nombre;
 
-    //
+    private static int contadorIds = 1;
+    //constructor
     public Usuario(int id, String nombre) {
-        this.id = id;
+        this.id = contadorIds++;
         this.nombre = nombre;
     }
 }
